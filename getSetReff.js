e16532adba61fdce9ae3cdd82ddead6e56a9d5c7
Boris <boris@boris-VirtualBox.(none)>
2014-05-09 13:46:38 +0300

    minor logic changes and bug fix for page refresh

--- getSetReff.js
+++ getSetReff.js
@@ -1,5 +1,42 @@
 function getSetReff()
 {
+
+    if (!Array.prototype.filter) //Array.filter() isn't included in IE until version 9.
+    {
+      Array.prototype.filter = function(fun /*, thisArg */)
+      {
+        "use strict";
+
+        if (this === void 0 || this === null)
+          throw new TypeError();
+
+        var t = Object(this);
+        var len = t.length >>> 0;
+        if (typeof fun !== "function")
+          throw new TypeError();
+
+        var res = [];
+        var thisArg = arguments.length >= 2 ? arguments[1] : void 0;
+        for (var i = 0; i < len; i++)
+        {
+          if (i in t)
+          {
+            var val = t[i];
+
+            // NOTE: Technically this should Object.defineProperty at
+            //       the next index, as push can be affected by
+            //       properties on Object.prototype and Array.prototype.
+            //       But that method's new, and collisions should be
+            //       rare, so use the more-compatible alternative.
+            if (fun.call(thisArg, val, i, t))
+              res.push(val);
+          }
+        }
+
+        return res;
+      };
+    }
+
     var _reff=[];
     _reff = dataLayer.filter(function (value) {if (value.setDomain)  return value; });
     if (_reff.length === 0) _reff[0]={'setDomain':document.location.hostname};
@@ -21,17 +58,13 @@ function getSetReff()
     //referrer or params?
     if (document.location.search.indexOf(__cmp) != -1 || document.location.search.indexOf(__mdm) != -1 || document.location.search.indexOf(__srcs) != -1)
     {
-        __gsr = "campaign://c:["+gcP(__cmp)+"]m:["+gcP(__mdm)+"]s:["+gcP(__srcs)+"]";
-    }
-    else if ((document.referrer == "" || document.referrer.indexOf(_reff[0].setDomain)!=-1) && __apc.split("|")[__apc.split("|").length-1].indexOf("c:") != -1)
-    {
-        __gsr = "campaign://"+(__apc.split("|")[__apc.split("|").length-1]).split("&")[0];
+        __gsr = "//campaign::c:["+gcP(__cmp)+"]m:["+gcP(__mdm)+"]s:["+gcP(__srcs)+"]";
     }
     else { __gsr = document.referrer; }
-
+    console.log(__gsr);
     //get referrer domain & verify adwords
-    __gsr = ((document.location.search.indexOf("gclid") != -1 && document.referrer != "") ? __gsr= __gsr.split('/')[2]+":[adwords]" : __gsr.split('/')[2]) 
-    __gsr = ((__gsr != "" && typeof __gsr != "undefined") ? __gsr : ""); 
+    __gsr = ((document.location.search.indexOf("gclid") != -1) ? "//campaign::[adwords]" : __gsr); 
+    __gsr = ((typeof __gsr == "undefined" || __gsr == "" || __gsr.indexOf(_reff[0].setDomain)!=-1) ? "(direct)" : __gsr.split('/')[2]);
 
     if (__asc)
     {
@@ -50,27 +83,29 @@ function getSetReff()
         sC("__sreff",__asc);
     }
     
-    // if refferer is not current domain
-    if (__nwss == 1 || __gsr.indexOf(_reff[0].setDomain)==-1)
+
+
+    //if new session
+    if (__nwss == 1)
     {
-        __rf = ((__gsr == "" || __gsr.indexOf(_reff[0].setDomain)!=-1) ? "(direct)" : __gsr);      
-        t__apc = __apc.split("|");
-        __tpc = t__apc[t__apc.length-1];
-        __tpc=__tpc.split("&")[0];
-        if (__nwss == 1 || __rf != __tpc) { res__apc = (__apc != "" ? __apc+"|" : ""); __tpc = __rf; sC("__reff",res__apc+__tpc+"&"+__asc,180);}
+        res__apc = (__apc != "" ? __apc+"|" : ""); 
+        sC("__reff",res__apc+__gsr+"&"+__asc,730);
     }
     else
     {
         t__apc = __apc.split("|");
         __tpc = t__apc[t__apc.length-1];
-        __tpc=__tpc.split("&")[0] != "" ? __tpc.split("&")[0] : "(direct)";
-        __tpc = __tpc.indexOf(_reff[0].setDomain)!=-1 ? __tpc : "(direct)";
+        __tpc=__tpc.split("&")[0] != "" ? __tpc.split("&")[0] : __gsr;
         res__apc = (t__apc.length == 1 ? "" : (t__apc.slice(0,-1).join("|")+"|"));
-        sC("__reff",res__apc+__tpc+"&"+__asc,730);
-    }
-    
 
+        if (__gsr == "(direct)" || __gsr.indexOf(__tpc.split("&")[0])!=-1) 
+        {
+            sC("__reff",res__apc+__tpc+"&"+__asc,730);
+        }
+        else
+        {
+            sC("__reff",__apc+"|"+__gsr+"&"+__asc,730);
+        } 
+    }
     return rC("__reff");
 }
-
-
